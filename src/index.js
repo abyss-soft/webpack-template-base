d7ae53720aad12689383af24b930f440aa489278
Alexander Panov <abyss-soft@yandex.ru>
2020-07-18 22:27:00 +1000

    feat: smart-grid

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
 import "./js/common.js";
 import "./assets/css/main.css";
-import './assets/scss/main.scss';
-
-let i = 0;
-let y;
-
-for (let x = 0; x < 10; x++) {
-  i = i + x;
-  const xx = i * x;
-}
+import "./assets/scss/main.scss";
